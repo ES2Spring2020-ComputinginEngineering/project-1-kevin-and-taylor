f6c7f2097e972af6f220a80f26558d8c99affa2c
kzhang707 <kzhang707@gmail.com>
2020-03-05 17:51:50 -0500

    it works now!
    makes 5 separate graphs, each with 3 subplots for x acc, y acc, and theta vs time
    makes 1 additional graph for period vs time

--- Step 4/PlotData.py
+++ Step 4/PlotData.py
@@ -1,25 +1,35 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  3 15:30:20 2020
-
-@author: kishi
-"""
+#INSERT HEADER
 
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import csv
+import scipy.signal as sig
 
-t=[] 
-x=[]
-y=[]
-z=[]
-theta = []
-
-def plotdata (filename, lowerbound, upperbound):
+def average (array):
+    # takes an array and finds the average distance between indices of said array
+    # used to calculate average period length 
+    differences = []
+    arraylist = list(array)
+    i = 1
+    while i < len(arraylist):
+        differences.append (arraylist[i] - arraylist[i-1])
+        i += 1
+    else:
+        array2 = np.array(differences)
+        return (np.average(array2))
+    
+#REMEMBER TO COMMENT (AND LIKE AND SUBSCRIBE AND SMASH THAT BELL)
+def parsedata (filename, lowerbound, upperbound):
+    t=[] 
+    x=[]
+    y=[]
+    theta = []
     with open(filename, 'r') as csvfile:
         plots= csv.reader(csvfile, delimiter=',')
         for row in plots:
-            if not((len(row[0]) == 0) or (len(row[1]) == 0) or (len(row[2]) == 0)): 
+            if not(len(row[0]) == 0): 
                 seconds = float(row[0])/1000
                 if lowerbound <= seconds <= upperbound:
                     t.append(seconds) 
@@ -30,25 +40,94 @@ def plotdata (filename, lowerbound, upperbound):
                     z_accel = float(row[3])
                     x_angle = math.atan2(x_accel,math.sqrt((y_accel ** 2) + (z_accel ** 2)))
                     theta.append (x_angle) 
-    print (filename)
-    plt.plot(t,x, marker='o')
-    plt.title('Time vs Acceleration X')
-    plt.xlabel('Time (seconds)')
-    plt.ylabel('Acceleration X in/second^2')
-    plt.show()
-    plt.plot(t,y, marker='o')
-    plt.title('Time vs Acceleration Y')
-    plt.xlabel('Time (seconds)')
-    plt.ylabel('Acceleration Y in/second^2')
-    plt.show()
-    plt.plot(t,theta, marker='o')
-    plt.title('Time vs Angle')
-    plt.xlabel('Time (seconds)')
-    plt.ylabel('Angle (radians)')
-    plt.show()
+        theta_array = np.array(theta)
+        theta_filt = sig.medfilt(theta_array)
+        theta_peaks,_ = sig.find_peaks (theta_filt, distance=25)
+        time = np.array(t)
+        time_peaks = time[theta_peaks]
+        results = [time, x, y, theta, average(time_peaks)]
+        return results
 
-#plotdata ('19DATAPENDULUM.csv',8,12)
-#plotdata ('17DATAPENDULUM.csv',8,12)
-#plotdata ('15DATAPENDULUM.csv',8,12)
-#plotdata ('13DATAPENDULUM.csv',8,12)
-plotdata ('11DATAPENDULUM.csv',8,12)
+
+def graphdata ():
+    lemgths = [19,17,15,13,11]
+    periods = []
+    
+    parseddata = parsedata ('19DATAPENDULUM.csv',10,18)
+    t = parseddata [0]
+    x = parseddata [1]
+    y = parseddata [2]
+    theta = parseddata [3] 
+    periods.append(parseddata [4])
+    fig, (ax1, ax2, ax3) = plt.subplots(3, figsize = [9,6],sharex=True)
+    ax1.plot(t, x)
+    ax1.set_title('Time vs X-Acc')
+    ax2.plot(t, y, 'tab:orange')
+    ax2.set_title('Time vs Y-Acc')
+    ax3.plot(t,theta)
+    ax3.set_title('Time vs Theta')
+    
+    parseddata = parsedata ('17DATAPENDULUM.csv',10,18)
+    t = parseddata [0]
+    x = parseddata [1]
+    y = parseddata [2]
+    theta = parseddata [3] 
+    periods.append(parseddata [4])
+    fig, (ax1, ax2, ax3) = plt.subplots(3, figsize = [9,6],sharex=True)
+    ax1.plot(t, x)
+    ax1.set_title('Time vs X-Acc')
+    ax2.plot(t, y, 'tab:orange')
+    ax2.set_title('Time vs Y-Acc')
+    ax3.plot(t,theta)
+    ax3.set_title('Time vs Theta')
+    
+    parseddata = parsedata ('15DATAPENDULUM.csv',10,18)
+    t = parseddata [0]
+    x = parseddata [1]
+    y = parseddata [2]
+    theta = parseddata [3]     
+    periods.append(parseddata [4])
+    fig, (ax1, ax2, ax3) = plt.subplots(3, figsize = [9,6],sharex=True)
+    ax1.plot(t, x)
+    ax1.set_title('Time vs X-Acc')
+    ax2.plot(t, y, 'tab:orange')
+    ax2.set_title('Time vs Y-Acc')
+    ax3.plot(t,theta)
+    ax3.set_title('Time vs Theta')
+    
+    parseddata = parsedata ('13DATAPENDULUM.csv',12,18)
+    t = parseddata [0]
+    x = parseddata [1]
+    y = parseddata [2]
+    theta = parseddata [3] 
+    periods.append(parseddata [4])
+    fig, (ax1, ax2, ax3) = plt.subplots(3, figsize = [9,6],sharex=True)
+    ax1.plot(t, x)
+    ax1.set_title('Time vs X-Acc')
+    ax2.plot(t, y, 'tab:orange')
+    ax2.set_title('Time vs Y-Acc')
+    ax3.plot(t,theta)
+    ax3.set_title('Time vs Theta')
+    
+    parseddata = parsedata ('11DATAPENDULUM.csv',10,18)
+    t = parseddata [0]
+    x = parseddata [1]
+    y = parseddata [2]
+    theta = parseddata [3] 
+    periods.append(parseddata [4])
+    fig, (ax1, ax2, ax3) = plt.subplots(3, figsize = [9,6],sharex=True)
+    ax1.plot(t, x)
+    ax1.set_title('Time vs X-Acc')
+    ax2.plot(t, y, 'tab:orange')
+    ax2.set_title('Time vs Y-Acc')
+    ax3.plot(t,theta)
+    ax3.set_title('Time vs Theta')
+    
+    plt.figure()
+    plt.plot(lemgths, periods)
+    plt.plot(lemgths, periods, 'b.')
+    plt.show()
+    
+# MAIN SCRIPT (REEEEEEE)
+    
+graphdata ()
